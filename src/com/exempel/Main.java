3b3074e8dd0097de9a2cdb2f27bface4c5e2b50c
Philip Mattsson <maini.ac@hotmail.com>
2020-08-27 14:27:21 +0200

    Version 5

--- src/com/exempel/Main.java
+++ src/com/exempel/Main.java
@@ -1,42 +1,45 @@
 package com.exempel;
 
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 	static Scanner scan = new Scanner(System.in);
 
-	static int[] highScores= new int[10];
-
+	static int[] highScore;
 
 	public static void main(String[] args) {
+
+		System.out.println("Hur många varv får det vara?");
+		int numberOfLaps = scan.nextInt();
+		highScore = new int [numberOfLaps];
+
 		diceGames();
+
 	}
 
 	static void diceGames(){
 
-		System.out.print("Hur många kast vill du göra? ");
-		int antalKast = scan.nextInt();
+		System.out.print("Hur många kast vill du ange? ");
+		int numberOfThrow = scan.nextInt();
 
-		int sumDator = 0;
-		int sumspelare = 0;
+		int sumComputer = 0;
+		int sumPlayer = 0;
 
-		for (int i = 0; i < antalKast; i++){
-			int datorskast = (int) (Math.random()*6+1);
-			int spelarenskast = (int) (Math.random()*6+1);
+		for (int i = 0; i < numberOfThrow; i++){
+			int computersThrow = (int) (Math.random()*6+1);
+			int playersThrow = (int) (Math.random()*6+1);
 
 
-			sumspelare += sumspelare+spelarenskast;
-			sumDator += sumDator+datorskast;
+			sumPlayer += sumPlayer+playersThrow;
+			sumComputer += sumComputer+computersThrow;
 
-			System.out.println("Spelar: " + spelarenskast);
-			System.out.println("Dator: " + datorskast);
+			System.out.println("Spelar: " + playersThrow);
+			System.out.println("Dator: " + computersThrow);
 			System.out.println();
 
 		}
-		System.out.println("Summa för spelare  är: " + sumspelare);
-		System.out.println("Summa för datorn är: " + sumDator);
+		System.out.println("Summa för spelare  är: " + sumPlayer);
+		System.out.println("Summa för datorn är: " + sumComputer);
 	}
 }
