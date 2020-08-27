4af96b5c0612b56f7f288eeea7de9a96acdb5dcc
Philip Mattsson <maini.ac@hotmail.com>
2020-08-27 16:07:16 +0200

    Version 6

--- src/com/exempel/Main.java
+++ src/com/exempel/Main.java
@@ -6,22 +6,37 @@ public class Main {
 
 	static Scanner scan = new Scanner(System.in);
 
-	static int[] highScore;
+	//static int[] highScore;
+
+	static String choice;
 
 	public static void main(String[] args) {
 
-		System.out.println("Hur många varv får det vara?");
+	/*	System.out.println("Hur många varv vill du köra?");
 		int numberOfLaps = scan.nextInt();
-		highScore = new int [numberOfLaps];
 
-		diceGames();
+		highScore = new int [numberOfLaps]; */
 
+		boolean exit = false;
+		System.out.println("Välkommen till Dicegame, starta?");
+
+		while (!exit){
+			choice = scan.nextLine().toLowerCase();
+
+			if(choice.equals("nej")){
+				exit = true;
+				System.out.println("Avslutar spelet nu...");
+			}else {
+				diceGames();
+			}
+		}
 	}
 
 	static void diceGames(){
 
 		System.out.print("Hur många kast vill du ange? ");
 		int numberOfThrow = scan.nextInt();
+		choice = scan.nextLine().toLowerCase();
 
 		int sumComputer = 0;
 		int sumPlayer = 0;
@@ -31,15 +46,25 @@ public class Main {
 			int playersThrow = (int) (Math.random()*6+1);
 
 
-			sumPlayer += sumPlayer+playersThrow;
-			sumComputer += sumComputer+computersThrow;
+			sumPlayer += sumPlayer + playersThrow;
+			sumComputer += sumComputer + computersThrow;
 
 			System.out.println("Spelar: " + playersThrow);
 			System.out.println("Dator: " + computersThrow);
 			System.out.println();
 
 		}
-		System.out.println("Summa för spelare  är: " + sumPlayer);
+
+		System.out.println("Summa för spelare är: " + sumPlayer);
 		System.out.println("Summa för datorn är: " + sumComputer);
+
+		if(sumPlayer > sumComputer){
+			System.out.println("Spelare vann");
+		}else {
+			System.out.println("Datorn vann");
+		}
+
+		System.out.println();
+		System.out.println("Fortsätta?");
 	}
 }
