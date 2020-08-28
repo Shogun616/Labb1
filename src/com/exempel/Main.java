3dbcd631bf01b4b016c58628d5eb980102312dce
Philip Mattsson <maini.ac@hotmail.com>
2020-08-28 15:39:26 +0200

    Version 8

--- src/com/exempel/Main.java
+++ src/com/exempel/Main.java
@@ -1,36 +1,15 @@
 package com.exempel;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 	static Scanner scan = new Scanner(System.in);
 
-	//static int[] highScore;
-
 	static String choice;
 
-	public static void main(String[] args) {
-
-	/*	System.out.println("Hur många varv vill du köra?");
-		int numberOfLaps = scan.nextInt();
-
-		highScore = new int [numberOfLaps]; */
-
-		boolean exit = false;
-		System.out.println("Välkommen till Dicegame, starta?");
-
-		while (!exit){
-			choice = scan.nextLine().toLowerCase();
-
-			if(choice.equals("nej")){
-				exit = true;
-				System.out.println("Avslutar spelet nu...");
-			}else {
-				diceGames();
-			}
-		}
-	}
+	static int[] highScore = new int[5];
 
 	static void diceGames(){
 
@@ -40,6 +19,7 @@ public class Main {
 
 		int sumComputer = 0;
 		int sumPlayer = 0;
+		int position = 0;
 
 		for (int i = 0; i < numberOfThrow; i++){
 			int computersThrow = (int) (Math.random()*6+1);
@@ -60,11 +40,60 @@ public class Main {
 
 		if(sumPlayer > sumComputer){
 			System.out.println("Spelare vann");
-		}else {
+			highScore[position] = sumPlayer;
+			position++;
+			scoreBoard();
+		}else if(sumPlayer < sumComputer){
 			System.out.println("Datorn vann");
+			highScore[position] = sumComputer;
+			position++;
+			scoreBoard();
+		} else {
+			System.out.println("Ovagjort");
 		}
 
 		System.out.println();
-		System.out.println("Fortsätta?");
+		System.out.println("Fortsätta? (ja/nej)");
+	}
+
+	static void scoreBoard(){
+
+		//int min = 0;
+		Arrays.sort(highScore);
+
+		System.out.println();
+		System.out.println("High Score");
+
+
+		for (int i = highScore.length-1; i >=0; i--) {
+			System.out.println(highScore[i]);
+		}
+
+	/*	for(int j = 1; j < highScore.length; j++){
+			if(highScore[j] < highScore[min]){
+				min = j;
+			}
+		}*/
+	}
+
+	public static void main(String[] args) {
+
+		boolean exit = false;
+		System.out.println("Välkommen till Dicegame, starta? (ja/nej)");
+
+		while (!exit){
+			choice = scan.nextLine().toLowerCase();
+
+			if(choice.equals("nej")){
+				exit = true;
+				System.out.println("Avslutar spelet nu...");
+			}
+			else if (choice.equals("ja")){
+				diceGames();
+			}else {
+				exit = true;
+				System.out.println("Ett problem har uppståt...");
+			}
+		}
 	}
 }
