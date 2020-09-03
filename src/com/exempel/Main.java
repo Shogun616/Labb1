cb29a700d65c87a73f733d61b8ccd6d0e74b2236
Philip Mattsson <maini.ac@hotmail.com>
2020-09-03 16:44:25 +0200

    Version 11.5

--- src/com/exempel/Main.java
+++ src/com/exempel/Main.java
@@ -5,227 +5,203 @@ import java.util.Scanner;
 
 public class Main {
 
-	static Scanner scan = new Scanner(System.in);
+    static Scanner scan = new Scanner(System.in);
 
-	static int[] highScore = new int[5];
+    static int[] highScore = new int[5];
 
-	static boolean repeat = true;
+    static boolean repeat = true;
 
-	static boolean exit = false;
+    static boolean exit = false;
 
-	static boolean choice = false;
+    static String decision;
 
-	static String decision;
+    static int singleOrMulti;
 
-	static int counter = 5;
+    static void diceGames() {
 
-	static void diceGames(){
-        // Choose number of dice
-		System.out.print("Hur många kast vill du ange? ");
-		int numberOfThrow = scan.nextInt();
-		decision = scan.nextLine();
+        System.out.print("Hur många kast vill du ange? ");
+        int numberOfThrow = scan.nextInt();
+        scan.nextLine();
 
-		int sumComputer = 0;
-		int sumPlayer = 0;
-		int position = 0;
+        int sumComputer = 0;
+        int sumPlayer = 0;
+        int position = 0;
 
-		for (int i = 1; i < numberOfThrow; i++){
-			int computersThrow = (int) (Math.random()*6+1);
-			int playersThrow = (int) (Math.random()*6+1);
+        for (int i = 0; i < numberOfThrow; i++) {
+            int computersThrow = (int) (Math.random() * 6 + 1);
+            int playersThrow = (int) (Math.random() * 6 + 1);
 
+            sumPlayer = sumPlayer + playersThrow;
+            sumComputer = sumComputer + computersThrow;
 
-			sumPlayer = sumPlayer + playersThrow;
-			sumComputer = sumComputer + computersThrow;
+            System.out.println("Spelar");
+            System.out.println("Tärningskast: " + playersThrow);
+            System.out.println("Dator");
+            System.out.println("Tärningskast: " + computersThrow);
+            System.out.println();
+        }
 
-			System.out.println("Spelar");
-			System.out.println("Tärningskast: " + playersThrow);
-			System.out.println("Dator");
-			System.out.println("Tärningskast: " + computersThrow);
-			System.out.println();
-		}
+        System.out.println("Summa för spelare är: " + sumPlayer);
+        System.out.println("Summa för datorn är: " + sumComputer);
 
-		System.out.println("Summa för spelare är: " + sumPlayer);
-		System.out.println("Summa för datorn är: " + sumComputer);
+        if (sumPlayer > sumComputer) {
+            System.out.println("Spelare vann");
+            highScore[position] = sumPlayer;
+            position++;
+            scoreBoard();
+        } else if (sumPlayer < sumComputer) {
+            System.out.println("Datorn vann");
+            highScore[position] = sumComputer;
+            position++;
+            scoreBoard();
+        } else {
+            System.out.println("Ovagjort");
+        }
 
-		// an if-statement that will print the results
-		if(sumPlayer > sumComputer){
-			System.out.println("Spelare vann");
-			highScore[position] = sumPlayer;
-			position++;
-			scoreBoard();
-		}else if(sumPlayer < sumComputer){
-			System.out.println("Datorn vann");
-			highScore[position] = sumComputer;
-			position++;
-			scoreBoard();
-		} else {
-			System.out.println("Ovagjort");
-		}
+        System.out.println();
+        System.out.println("Fortsätta (ja/nej)");
+        choice();
+    }
 
-		System.out.println();
-		System.out.println("Fortsätta (ja/nej)");
-		choice();
-	}
+    static void diceGames2() {
+        System.out.println("Ange antal spelare: ");
+        int numberOfPlayer = scan.nextInt();
+        scan.nextLine();
 
-	static void diceGames2(){
-		System.out.println("Ange antal spelare: ");
-		int numberOfPlayer = scan.nextInt();
+        System.out.print("Hur många kast vill du ange? ");
+        int numberOfThrow = scan.nextInt();
+        scan.nextLine();
 
-		System.out.print("Hur många kast vill du ange? ");
-		int numberOfThrow = scan.nextInt();
-		decision = scan.nextLine();
+        int position = 0;
 
-		int position = 0;
+        int[] sumPlayer = new int[6];
 
-		int[] sumPlayer = new int[6];
+        for (int i = 1; i <= numberOfPlayer; i++) {
+            System.out.println("Spelare " + i + ".");
 
-		for(int i=1; i <= numberOfPlayer; i++) {
-			System.out.println("Spelare " + i + ".");
+            for (int j = 1; j < numberOfThrow; j++) {
+                System.out.println("Tärningskast " + j + " (spelare: " + i + ").");
+                int playersThrow = (int) (1 + Math.random() * 6);
 
-			for(int j = 1; j < numberOfThrow; j++) {
-				System.out.println("Tärningskast " + j + " (spelare: " + i + ").");
-				int playersThrow = (int) (1+Math.random()*6);
+                System.out.println("Spelare: " + i + " kast: " + playersThrow);
 
-				System.out.println("Spelare: " + i + " kast: " + playersThrow);
+                sumPlayer[i] = sumPlayer[i] + playersThrow;
+            }
+            System.out.println("Summa spelare " + i + ": " + sumPlayer[i]);
+            System.out.println();
+            highScore[position] = sumPlayer[i];
+            position++;
+            scoreBoard();
+            System.out.println();
+        }
 
-				sumPlayer[i] = sumPlayer[i] + playersThrow;
-			}
-			System.out.println("Summa spelare " + i + ": " + sumPlayer[i]);
-			System.out.println();
-			highScore[position] = sumPlayer[i];
-			position++;
-			scoreBoard();
-			System.out.println();
-		}
+        System.out.println();
+        System.out.println("Fortsätta (ja/nej)");
+        choice();
+    }
 
-		System.out.println();
-		System.out.println("Fortsätta (ja/nej)");
-		choice2();
-	}
+    static void scoreBoard() {
 
-	static void scoreBoard(){
+        Arrays.sort(highScore);
 
-		Arrays.sort(highScore);
-
-		System.out.println();
-		System.out.println("High Score");
-		System.out.println("==========");
+        System.out.println();
+        System.out.println("High Score");
+        System.out.println("==========");
 
 
-		for (int i = highScore.length-1; i >=0; i--) {
-			System.out.println(highScore[i]);
+        for (int i = highScore.length - 1; i >= 0; i--) {
+            System.out.println(highScore[i]);
+        }
+    }
 
-			int min = 0;
-			for(int j = 1; j < counter; j++){
-				if(highScore[j] < highScore[min]){
-					min = j;
-				}
-			}
-		}
-	}
+    static void menu() {
+        System.out.println("\n======================================");
+        System.out.println("              Main Menu             ");
+        System.out.println("======================================");
+        System.out.println("1. Player VS Computer");
+        System.out.println("2. Player Vs Player");
+        System.out.println("3. HighScore");
+        System.out.println("0. Exit");
 
-	static void menu(){
-		System.out.println("\n======================================");
-		System.out.println("              Main Menu             ");
-		System.out.println("======================================");
-		System.out.println("1. Player VS Computer");
-		System.out.println("2. Player Vs Player");
-		System.out.println("3. HighScore");
-		System.out.println("0. Exit");
+        System.out.println("\nMake your choice");
+        singleOrMulti = scan.nextInt();
+        scan.nextLine();
 
-		System.out.println("\nMake your choice");
-		int choice = scan.nextInt();
-		scan.nextLine();
+        switch (singleOrMulti) {
 
-		switch (choice){
+            case 0:
+                System.out.println("Avsluta? (ja/nej)");
+                options();
+                repeat = false;
+                System.out.println("Avslutar spelet nu...");
+                break;
 
-			case 0:
-				System.out.println("Avsluta? (ja/nej)");
-				options();
-				repeat = false;
-				System.out.println("Avslutar spelet nu...");
-				break;
+            case 1:
+                System.out.println("Välkommen till Dicegame PvE");
+                diceGames();
+                break;
 
-			case 1:
-				System.out.println("Välkommen till Dicegame PvE");
-				diceGames();
-				break;
+            case 2:
+                System.out.println("Välkommen till DiceGame PvP");
+                diceGames2();
+                break;
 
-			case 2:
-				System.out.println("Välkommen till DiceGame PvP");
-				diceGames2();
-				break;
+            case 3:
+                scoreBoard();
+                System.out.println();
+                System.out.println("Press Enter to show menu");
+                scan.nextLine();
+                menu();
+                break;
 
-			case 3:
-				scoreBoard();
-				System.out.println();
-				System.out.println("Press Enter to show menu");
-				scan.nextLine();
-				menu();
-				break;
+            default:
+                System.out.println("Ogiltig val!");
+        }
+    }
 
-			default:
-				System.out.println("Ogiltig val!");
-		}
-	}
+    static void options() {
 
-	static void options(){
+        while (!exit) {
+            decision = scan.nextLine();
 
-		while (!exit) {
-			decision = scan.nextLine().toLowerCase();
+            if (decision.equalsIgnoreCase("nej")) {
+                exit = true;
+                menu();
+            } else if (decision.equalsIgnoreCase("ja")) {
+                exit = true;
+            }
+        }
+    }
 
-			if (decision.equals("nej")) {
-				exit = true;
-				menu();
-			} else if (decision.equals("ja")) {
-				exit = true;
-			}
-		}
-	}
+    static void choice() {
 
-	static void choice(){
-		while (!choice){
-			decision = scan.nextLine().toLowerCase();
+        boolean choice = true;
 
-			switch (decision) {
-				case "nej":
-					choice = true;
-					menu();
-					break;
-				case "ja":
-					diceGames();
-					break;
-				default:
-					choice = true;
-					System.out.println("Ett problem har uppståt...");
-					break;
-			}
-		}
-	}
+        while (choice) {
+            decision = scan.nextLine().toLowerCase();
 
-	static void choice2(){
-		while (!choice){
-			decision = scan.nextLine().toLowerCase();
+            switch (decision) {
+                case "nej":
+                    choice = false;
+                    menu();
+                    break;
+                case "ja":
+                    if (singleOrMulti == 1)
+                        diceGames();
+                    else if (singleOrMulti == 2)
+                        diceGames2();
+                    break;
+                default:
+                    choice = false;
+                    System.out.println("Ett problem har uppståt...");
+                    break;
+            }
+        }
+    }
 
-			switch (decision) {
-				case "nej":
-					choice = true;
-					menu();
-					break;
-				case "ja":
-					diceGames2();
-					break;
-				default:
-					choice = true;
-					System.out.println("Ett problem har uppståt...");
-					break;
-			}
-		}
-	}
-
-	public static void main(String[] args) {
-		while (repeat){
-			menu();
-		}
-	}
+    public static void main(String[] args) {
+        while (repeat) {
+            menu();
+        }
+    }
 }
